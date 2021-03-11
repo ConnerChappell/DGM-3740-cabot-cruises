90dd658210625e8052cff671bc978e678aa27f3a
Conner Chappell <conner.chappell123@gmail.com>
2021-03-11 16:18:30 -0700

    added agent 3 php page to files

---
+++ agent3.php
@@ -0,0 +1,37 @@
+<h2 class="agentHeader">Agent - Susan Walker</h2>
+
+    <div class="agentDiv clearfix">
+        <div class="agentImage">
+            <img src="../photos/agent3.jpg" alt="Susan Walker Portrait">
+        </div>
+        <div class="agentPhone">
+            <h4>Phone:</h4>
+            <p>1-800-555-1234 (ext. 5)</p>
+        </div>
+        <div class="agentWorking">
+            <h4>Working:</h4>
+            <p>Monday, Tuesday, Thursday</p>
+        </div>
+        <div class="agentSpecialization">
+            <h4>Destination Specialization:</h4>
+            <p>Kauai Island, Maui Island, California, Las Vegas, Bahamas, Caribbean</p>
+        </div>
+        <div class="agentHours">
+            <h4>Office Hours:</h4>
+            <p>9:00am - 5:00pm</p>
+        </div>
+        <div class="agentBiography">
+            <h4>Biography:</h4>
+            <p>For me, planning a vacation is sometimes just as fun as the actual trip. I love daydreaming about the possibilities of the upcoming holiday, and I spend endless hours researching travel guides and reading online reviews as I create the next itinerary. Whether you are a traveler looking for relaxation and romance, or you are hoping to embark on a great adventure, I can create a travel plan suited to your needs.</p>
+            <p>I genuinely enjoy helping fellow travelers check off destinations from their bucket lists. I combine my knowledge of travel destinations with my professional experience as a travel planner to create an unforgettable vacation.</p>
+        </div>
+        <div class="customerComments">
+            <h4>Customer Comments:</h4>
+            <p>"She was so easy to work with and SO HELPFUL. We had activities to do everyday with her travel guide. Our trip was truly incredible with her help! She also checked in on us throughout to make sure the trip was going well, and if I had questions she was easy to get a hold of!"<br>
+            -Morgan</p>
+            <p>"Susan is very thorough with her research so she can provide a great trip for her clients. She is easy to work with and very responsive."<br>
+            -Kyler</p>
+            <p>"Susan was amazing. She got our flights and hotel booked for us and made it a stress free trip for us! Our hotel was amazing and our flights were all very close so we didn’t have terrible layovers. I would recommend Susan to any of my friends or family booking trips!"<br>
+            -Cydee Nielsen</p>
+        </div>
+    </div>
